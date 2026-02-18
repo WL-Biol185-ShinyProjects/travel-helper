b6d3d8c711bd21eabc761a45556a32acb497592f
Maddie Montez <mmontez@mail.wlu.edu>
2026-02-18 12:59:10 -0500

    trying to rename ambiguous visa requirements

--- server.R
+++ server.R
@@ -4,6 +4,10 @@ library(tidyverse)
 
 passport_info <- read.csv("passport-index-tidy.csv")
 
+my_names <- c("90 Days Visa Free", "30 Days Visa Free")
+names(my_names) <- c(90, 30)
+passport_info$Requirement <- my_names(passport_info$Requirement)
+
 function(input, output) {
   output$Requirement <- renderText({
     result <- passport_info %>% 
