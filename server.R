bc7c358d12e02726f70befd022bd0f011a0d5dde
Lily Caldwell <lcaldwell@mail.wlu.edu>
2026-02-18 13:40:49 -0500

    commiting before trying to pull

--- server.R
+++ server.R
@@ -4,6 +4,10 @@ library(tidyverse)
 
 passport_info <- read.csv("passport-index-tidy.csv")
 
+Requirement_new <- c("90 Days Visa Free","30 Days Visa Free")
+names(Requirement_new) <- c("90", "30")
+passport_info$Requirement <- Requirement_new(passport_info$Requirement)
+
 function(input, output) {
   output$Requirement <- renderText({
     result <- passport_info %>% 
