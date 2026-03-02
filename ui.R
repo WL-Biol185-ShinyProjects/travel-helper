d09b6ae585156756baf84ccdc379cc835294bc49
Lily Caldwell <lcaldwell@mail.wlu.edu>
2026-03-02 13:50:11 -0500

    commiting again dashoard sidebar

--- ui.R
+++ ui.R
@@ -1,22 +1,78 @@
 library(shiny)
+library(shinydashboard)
 
-fluidPage(
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectizeInput("Passport", 
-                     label = "Your Passport",
-                     choices = passport_info$Passport
-        
-      ),
-      selectizeInput("Destination", 
-                     label = "Your Destination",
-                     choices = passport_info$Destination
-    )
+# Sidebar content
+dashboardSidebar(
+  sidebarMenu(
+    menuItem("Welcome",            tabName = "welcome"),
+    menuItem("International Travel", tabName = "international_travel"),
+    menuItem("Weather",            tabName = "weather"),
+    menuItem("Airports",           tabName = "airports"),
+    menuItem("Airlines",           tabName = "airlines"),
+    menuItem("Pricing",            tabName = "pricing"),
+    menuItem("Travel Suggestions", tabName = "travel_suggestions")
+  )
+)
+
+# Body content
+dashboardBody(
+  tabItems(
+    
+    # Welcome tab
+    tabItem(tabName = "welcome",
+            fluidRow(
+              box(),
+              box()
+            )
     ),
-    mainPanel(
-      h4("Requirement"), 
-      verbatimTextOutput("Requirement")
+    
+    # International Travel tab
+    tabItem(tabName = "international_travel",
+            fluidRow(
+              box(),
+              box()
+            )
+    ),
+    
+    # Weather tab
+    tabItem(tabName = "weather",
+            fluidRow(
+              box(),
+              box()
+            )
+    ),
+    
+    # Airports tab
+    tabItem(tabName = "airports",
+            fluidRow(
+              box(),
+              box()
+            )
+    ),
+    
+    # Airlines tab
+    tabItem(tabName = "airlines",
+            fluidRow(
+              box(),
+              box()
+            )
+    ),
+    
+    # Pricing tab
+    tabItem(tabName = "pricing",
+            fluidRow(
+              box(),
+              box()
+            )
+    ),
+    
+    # Travel Suggestions tab
+    tabItem(tabName = "travel_suggestions",
+            fluidRow(
+              box(),
+              box()
+            )
     )
+    
   )
 )
