2413ee07d262829e4c3708311c3f81bf4903c82e
Maddie Montez <mmontez@mail.wlu.edu>
2026-03-04 13:21:14 -0500

    committing to avoid merge issue

--- server.R
+++ server.R
@@ -2,19 +2,11 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 
+passport_info <- read.csv("passport-index-tidy.csv") 
 
 function(input, output) {
   
-<<<<<<< HEAD
-passport_info <- read.csv("passport-index-tidy.csv") 
 
 }
-=======
-  
-passport_info <- read.csv("passport-index-tidy.csv") 
 
-<<<<<<< HEAD
-}
-=======
->>>>>>> 4b415f24cccaba10e57946b7d3b93711b88d99ab
->>>>>>> d9e1551ae70852924c8bbc8ab4d6ada1a5dc63f3
+
