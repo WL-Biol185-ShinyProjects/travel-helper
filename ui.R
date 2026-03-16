8070332b86c27d2d26b8935018d51b0c8094fecd
Mac Palmer <mpalmer@mail.wlu.edu>
2026-03-16 12:57:21 -0400

    fixes

--- ui.R
+++ ui.R
@@ -6,15 +6,14 @@ library(dplyr)
 library(plotly)
 library(leaflet)
 
-
 # Data loading
 passport_info <- read.csv("passport-index-tidy.csv")
 currencyVcountry <- read.csv("currencyVcountry.csv")
 vaccinationVcountry <- read.csv("vaccinationVcountry_correct.csv")
 arrival_2025 <- read_excel("arrival information 2025.xlsx")
 adapter_data <- read.csv("travel_adapter_converter.csv")
-  colnames(arrival_2025) <- c("rank", "airport", "pct_on_time")
-  arrival_2025$airport <- reorder(arrival_2025$airport, arrival_2025$pct_on_time)
+colnames(arrival_2025) <- c("rank", "airport", "pct_on_time")
+arrival_2025$airport <- reorder(arrival_2025$airport, arrival_2025$pct_on_time)
 UNESCO <- read_excel("UNESCO_World_Heritage_Sites.xlsx")
 airfare_data <- read.csv("Consumer_Airfare_Report__Table_1_-_Top_1,000_Contiguous_State_City-Pair_Markets_20260309.csv") %>%
   mutate(
@@ -24,116 +23,66 @@ airfare_data <- read.csv("Consumer_Airfare_Report__Table_1_-_Top_1,000_Contiguou
     city1    = trimws(city1),
     city2    = trimws(city2)
   )
+
 carrier_names <- c(
-  "AA" = "American Airlines",
-  "AS" = "Alaska Airlines",
-  "B6" = "JetBlue Airways",
-  "DL" = "Delta Air Lines",
-  "F9" = "Frontier Airlines",
-  "G4" = "Allegiant Air",
-  "HA" = "Hawaiian Airlines",
-  "NK" = "Spirit Airlines",
-  "OO" = "SkyWest Airlines",
-  "UA" = "United Airlines",
-  "WN" = "Southwest Airlines",
-  "YX" = "Republic Airways",
-  "YV" = "Mesa Airlines",
-  "QX" = "Horizon Air",
-  "SY" = "Sun Country Airlines",
-  "EV" = "ExpressJet",
-  "CO" = "Continental Airlines",
-  "DH" = "Independence Air",
-  "FL" = "AirTran Airways",
-  "HP" = "America West Airlines",
-  "NW" = "Northwest Airlines",
-  "TW" = "Trans World Airlines",
-  "TZ" = "ATA Airlines",
-  "US" = "US Airways",
-  "VX" = "Virgin America",
-  "3M" = "Silver Airways",
-  "9N" = "Trans States Airlines",
-  "A7" = "Air Midwest",
-  "E9" = "Boston-Maine Airways",
-  "FF" = "Tower Air",
-  "J7" = "Valujet Airlines",
-  "JI" = "Midway Airlines",
-  "KP" = "Kiwi International Air Lines",
-  "KW" = "Carnival Air Lines",
-  "L4" = "Mountain Air Express",
-  "MX" = "Mexicana",
-  "N5" = "Nolinor Aviation",
-  "N7" = "National Airlines",
-  "NJ" = "Vanguard Airlines",
-  "OE" = "Westair Airlines",
-  "P9" = "Pro Air",
-  "PN" = "Pan American Airways",
-  "QQ" = "Reno Air",
-  "RP" = "Chautauqua Airlines",
-  "RU" = "Cape Air",
-  "SM" = "Sunjet International",
-  "SX" = "Skybus Airlines",
-  "T3" = "Eastern Airways",
-  "TB" = "USAir Shuttle",
-  "U5" = "USA 3000 Airlines",
-  "W7" = "Western Pacific Airlines",
-  "W9" = "Aloha Airlines",
-  "WV" = "Air South",
-  "XP" = "Casino Express",
-  "ZA" = "Access Air",
-  "ZW" = "Air Wisconsin"
+  "AA" = "American Airlines", "AS" = "Alaska Airlines", "B6" = "JetBlue Airways",
+  "DL" = "Delta Air Lines", "F9" = "Frontier Airlines", "G4" = "Allegiant Air",
+  "HA" = "Hawaiian Airlines", "NK" = "Spirit Airlines", "OO" = "SkyWest Airlines",
+  "UA" = "United Airlines", "WN" = "Southwest Airlines", "YX" = "Republic Airways",
+  "YV" = "Mesa Airlines", "QX" = "Horizon Air", "SY" = "Sun Country Airlines",
+  "EV" = "ExpressJet", "CO" = "Continental Airlines", "DH" = "Independence Air",
+  "FL" = "AirTran Airways", "HP" = "America West Airlines", "NW" = "Northwest Airlines",
+  "TW" = "Trans World Airlines", "TZ" = "ATA Airlines", "US" = "US Airways",
+  "VX" = "Virgin America", "3M" = "Silver Airways", "9N" = "Trans States Airlines",
+  "A7" = "Air Midwest", "E9" = "Boston-Maine Airways", "FF" = "Tower Air",
+  "J7" = "Valujet Airlines", "JI" = "Midway Airlines", "KP" = "Kiwi International Air Lines",
+  "KW" = "Carnival Air Lines", "L4" = "Mountain Air Express", "MX" = "Mexicana",
+  "N5" = "Nolinor Aviation", "N7" = "National Airlines", "NJ" = "Vanguard Airlines",
+  "OE" = "Westair Airlines", "P9" = "Pro Air", "PN" = "Pan American Airways",
+  "QQ" = "Reno Air", "RP" = "Chautauqua Airlines", "RU" = "Cape Air",
+  "SM" = "Sunjet International", "SX" = "Skybus Airlines", "T3" = "Eastern Airways",
+  "TB" = "USAir Shuttle", "U5" = "USA 3000 Airlines", "W7" = "Western Pacific Airlines",
+  "W9" = "Aloha Airlines", "WV" = "Air South", "XP" = "Casino Express",
+  "ZA" = "Access Air", "ZW" = "Air Wisconsin"
 )
 
 v <- c(
-  "90"            = "90 Days Visa Free",
-  "30"            = "30 Days Visa Free",
-  "60"            = "60 Days Visa Free",
-  "360"           = "360 Days Visa Free",
-  "21"            = "21 Days Visa Free",
-  "28"            = "28 Days Visa Free",
-  "19"            = "19 Days Visa Free",
-  "180"           = "180 Days Visa Free",
-  "14"            = "14 Days Visa Free",
-  "42"            = "42 Days Visa Free",
-  "15"            = "15 Days Visa Free",
-  "240"           = "240 Days Visa Free",
-  "120"           = "120 Days Visa Free",
-  "eta"           = "Electronic Travel Authorization",
-  "e-visa"        = "Electronic Visa Needed",
-  "visa required" = "Visa Required",
-  "visa on arrival" = "Visa on Arrival",
-  "visa free"     = "Visa Free",
-  "-1"            = "In-Country, No Visa Needed"
+  "90" = "90 Days Visa Free", "30" = "30 Days Visa Free", "60" = "60 Days Visa Free",
+  "360" = "360 Days Visa Free", "21" = "21 Days Visa Free", "28" = "28 Days Visa Free",
+  "19" = "19 Days Visa Free", "180" = "180 Days Visa Free", "14" = "14 Days Visa Free",
+  "42" = "42 Days Visa Free", "15" = "15 Days Visa Free", "240" = "240 Days Visa Free",
+  "120" = "120 Days Visa Free", "eta" = "Electronic Travel Authorization",
+  "e-visa" = "Electronic Visa Needed", "visa required" = "Visa Required",
+  "visa on arrival" = "Visa on Arrival", "visa free" = "Visa Free",
+  "-1" = "In-Country, No Visa Needed"
 )
 passport_info$Requirement <- recode(passport_info$Requirement, !!!v)
-
 
 world_cities <- read_excel("worldcities.xlsx")
 world_cities <- world_cities[!is.na(world_cities$population) & 
                                world_cities$population > 500000, ]
-# Rename in the dataframe
+
 airfare_data <- airfare_data %>%
   mutate(
     carrier_lg  = recode(carrier_lg,  !!!carrier_names),
     carrier_low = recode(carrier_low, !!!carrier_names)
   )
 
-#Dashboard page set up
 dashboardPage(
   dashboardHeader(title = "Travel Helper"),
-  # Sidebar content
+  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Welcome",            tabName = "welcome"),
+      menuItem("Welcome",              tabName = "welcome"),
       menuItem("International Travel", tabName = "international_travel"),
-      menuItem("Weather",            tabName = "weather"),
-      menuItem("Airports",           tabName = "airports"),
-      menuItem("Airlines",           tabName = "airlines"),
-      menuItem("Pricing",            tabName = "pricing"),
-      menuItem("Travel Suggestions", tabName = "travel_suggestions")
+      menuItem("Weather",              tabName = "weather"),
+      menuItem("Airports",             tabName = "airports"),
+      menuItem("Airlines",             tabName = "airlines"),
+      menuItem("Pricing",              tabName = "pricing"),
+      menuItem("Travel Suggestions",   tabName = "travel_suggestions")
     )
   ),
-
-  # Body content
+  
   dashboardBody(
     tabItems(
       
@@ -189,7 +138,6 @@ dashboardPage(
                     tags$li("✅ Discover sites to visit")
                   )
                 )
-
               )
       ),
       
@@ -198,53 +146,41 @@ dashboardPage(
               fluidRow(
                 box(
                   title = "What Travel Requirements do you Need?", status = "primary", solidHeader = TRUE,
-                  selectizeInput("Passport", 
+                  selectizeInput("Passport",
                                  label = "Your Passport",
-                                 choices = unique(passport_info$Passport)
-                                 
-                                ),
-                  selectizeInput("Destination", 
+                                 choices = unique(passport_info$Passport)),
+                  selectizeInput("Destination",
                                  label = "Your Destination",
-                                 choices = unique(passport_info$Destination)
-                                ),
-              
-                  h4("Requirement"), 
+                                 choices = unique(passport_info$Destination)),
+                  h4("Requirement"),
                   verbatimTextOutput("Requirement")
-                  
-                  ),
-                
+                ),
                 box(
                   title = "Currency Info", status = "primary", solidHeader = TRUE,
-                  selectizeInput("Country", 
+                  selectizeInput("Country",
                                  label = "Destination Country",
-                                 choices = (currencyVcountry$Country)
-                  ),
-                  h4("Currency"), 
+                                 choices = currencyVcountry$Country),
+                  h4("Currency"),
                   verbatimTextOutput("Currency")
                 ),
-                
                 box(
-                  title = "Vaccination Nedded", status = "primary", solidHeader = TRUE,
-                  selectizeInput("country_vaccination", 
-                                 label = "Country of destination",
-                                 choices = (vaccinationVcountry$country_vaccination)
-                  ),
-                  h4("Vaccination required"), 
+                  title = "Vaccination Needed", status = "primary", solidHeader = TRUE,
+                  selectizeInput("country_vaccination",
+                                 label = "Country of Destination",
+                                 choices = vaccinationVcountry$country_vaccination),
+                  h4("Vaccination Required"),
                   verbatimTextOutput("vaccination_required")
-                ), 
-                
+                ),
                 box(
                   title = "Electrical Adapter & Converter Requirements", status = "primary", solidHeader = TRUE,
                   selectizeInput("origin_country",
                                  label = "Country of Origin",
                                  choices = sort(unique(adapter_data$Origin.Country)),
-                                 options = list(placeholder = "Select your home country...")
-                  ),
+                                 options = list(placeholder = "Select your home country...")),
                   selectizeInput("dest_country",
                                  label = "Destination Country",
                                  choices = sort(unique(adapter_data$Destination.Country)),
-                                 options = list(placeholder = "Select your destination...")
-                  ),
+                                 options = list(placeholder = "Select your destination...")),
                   h4("Adapter Needed"),
                   verbatimTextOutput("adapter_needed"),
                   h4("Converter Needed"),
@@ -256,7 +192,6 @@ dashboardPage(
                 )
               )
       ),
-
       
       # Weather tab
       tabItem(tabName = "weather",
@@ -287,14 +222,14 @@ dashboardPage(
               )
       ),
       
-
       # Airports tab
       tabItem(tabName = "airports",
               fluidRow(
                 box(
                   title = "What is the Best Airport to Fly into?", status = "primary", solidHeader = TRUE,
                   width = 8,
-                  plotOutput("percentage_on_time", height = 400)),
+                  plotOutput("percentage_on_time", height = 400)
+                ),
                 box()
               )
       ),
@@ -310,33 +245,44 @@ dashboardPage(
       # Pricing tab
       tabItem(tabName = "pricing",
               fluidRow(
-                box(width = 4,
-                    title = "Search a Route",
-                    status = "primary",
-                    solidHeader = TRUE,
-                    selectizeInput("origin",
-                                   label = "Departure City",
-                                   choices = sort(unique(airfare_data$city1)),
-                                   options = list(placeholder = "Select departure city...")),
-                    uiOutput("dest_dropdown"),
-                    actionButton("search", "Search",
-                                 class = "btn-primary", width = "100%")
+                box(
+                  width = 4,
+                  title = "Search a Route", status = "primary", solidHeader = TRUE,
+                  selectizeInput("origin",
+                                 label = "Departure City",
+                                 choices = sort(unique(airfare_data$city1)),
+                                 options = list(placeholder = "Select departure city...")),
+                  uiOutput("dest_dropdown"),
+                  actionButton("search", "Search",
+                               class = "btn-primary", width = "100%")
                 ),
-                box(width = 8,
-                    title = "Route Information",
-                    status = "primary",
-                    solidHeader = TRUE,
-                    uiOutput("route_results"))
-                      )
-              ),
-  
-
+                box(
+                  width = 8,
+                  title = "Route Information", status = "primary", solidHeader = TRUE,
+                  uiOutput("route_results")
+                )
+              )
+      ),
+      
       # Travel Suggestions tab
       tabItem(tabName = "travel_suggestions",
               fluidRow(
-                box( ),
+                box(
+                  title = "Where Are You Going?", status = "primary", solidHeader = TRUE,
+                  selectizeInput("UNESCOCountry",
+                                 label = "Select Your Destination",
+                                 choices = sort(unique(UNESCO$Country)))
+                )
+              ),
+              fluidRow(
+                box(
+                  title = "UNESCO World Heritage Sites to Visit", status = "success", solidHeader = TRUE,
+                  width = 12,
+                  uiOutput("sites_table")
+                )
+              )
+      )
+      
+    )
   )
-  )
-)
-)
 )
